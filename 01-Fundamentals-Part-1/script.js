560c8abc26641c09889274ee6779b51f791e10cd
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-03 17:57:44 -0300

    Chapter 01 - Lesson 007: Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -6,6 +6,7 @@ Chapter 01 - Lesson 004: Linking a Javascript File
 /**********************************************************************************************
 Chapter 01 - Lesson 005: Values and Variables
 ***********************************************************************************************/
+/*
 //Values
 console.log("Gandalf");
 console.log(42);
@@ -31,3 +32,49 @@ let PI = 3.14159265; //name constants with all upper case letters
 //Name variables with descriptive names...
 let myFirstJob = "Programmer"; // Better than job1
 let currentJob = "Teacher"; //Better than job2
+*/
+
+/**********************************************************************************************
+Chapter 01 - Lesson 007: Data Types
+***********************************************************************************************/
+//Number: Floating point numbers - Used for decimals and integers
+let x = 9;
+let y = 2.4223;
+console.log(x);
+console.log(y);
+console.log(typeof x);
+
+//String: Sequence of characters - Used for text;
+let firstName = "Bildo";
+let lastName = "Baggins";
+console.log(firstName, lastName);
+console.log(typeof firstName);
+
+//Boolean: Logical type that can only be true or false - Used for taking decisions
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+
+//Undefined: Value taken by a variable that is not yet defined (empty value)
+let isRegister = undefined;
+console.log(isRegister);
+console.log(typeof isRegister);
+
+//Null: Also means 'empty value'
+let userId = null;
+console.log(userId);
+console.log(typeof userId); //historical bug where typeof null is object
+
+//Symbol (ES2015): Value that is unique and cannot be changed
+//BigInt (ES2020): Larger integers than the Number type can hold
+
+/*
+Javascript has dynamic type: We do not have to manually define the data type of the value stored
+in a variable. Instead, data types are determined automatically. Under the hood tha data type is
+assign to the value and not the variable.
+*/
+javaScriptIsFun = 42;
+console.log(typeof javaScriptIsFun);
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun);
