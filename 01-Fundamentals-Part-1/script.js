1ef6c47299cca2873615003086f7f8498c694106
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-03 17:17:56 -0300

    Chapter 01 - Lesson 005: Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,33 @@
 /**********************************************************************************************
-Chapter 01 - Lesson 04: Linking a Javascript File
+Chapter 01 - Lesson 004: Linking a Javascript File
 ***********************************************************************************************/
-console.log("Is Working!!!");
+// console.log("Is Working!!!");
+
+/**********************************************************************************************
+Chapter 01 - Lesson 005: Values and Variables
+***********************************************************************************************/
+//Values
+console.log("Gandalf");
+console.log(42);
+console.log(40 + 8 + 23 - 10);
+
+//Variables
+let firstName = "Frodo"; //camelCase convention
+let lastName = "Baggins";
+
+console.log(firstName);
+console.log(lastName);
+
+//Illegal Variable names
+// let 3years = 3; // variables cannot start with numbers...
+// let sam&rose = "SR"; // variables can only contain letters, number, underscore and dollar sign...
+// let new = 27 // variables cannot use reserved words...
+// let name = "Bilbo"; // name is a reserved word that are still allowed, but is not recommended to use...
+
+//Conventions
+let ringBearer = "Bilbo"; //name variables with camelCase
+let PI = 3.14159265; //name constants with all upper case letters
+
+//Name variables with descriptive names...
+let myFirstJob = "Programmer"; // Better than job1
+let currentJob = "Teacher"; //Better than job2
