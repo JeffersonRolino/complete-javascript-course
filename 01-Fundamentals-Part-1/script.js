d153a3fca296fd589efbef29265aff0e312e6d5e
jeffersonrolino <jefferson.rolino@gmail.com>
2025-04-26 08:08:46 -0300

    Chapter 01 - Lesson 025: Coding Challenge #4

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -538,23 +538,53 @@ Chapter 01 - Lesson 023: Statements VS Expressions
 // console.log(`${me} is ${2037 - 1991} years old`);
 
 /**********************************************************************************
-Chapter 01 - Lesson 023: Statements VS Expressions
+Chapter 01 - Lesson 024: Statements VS Expressions
 **********************************************************************************/
-const age = 21;
-// age >= 18
-//     ? console.log("I like to drink wine...")
-//     : console.log("I like to drink water...");
+// const age = 21;
+// // age >= 18
+// //     ? console.log("I like to drink wine...")
+// //     : console.log("I like to drink water...");
 
-const drink = age >= 18 ? "wine" : "water";
-// console.log(drink);
+// const drink = age >= 18 ? "wine" : "water";
+// // console.log(drink);
 
-let drink2;
-if (age >= 18) {
-    drink2 = "wine";
-} else {
-    drink2 = "water";
-}
+// let drink2;
+// if (age >= 18) {
+//     drink2 = "wine";
+// } else {
+//     drink2 = "water";
+// }
 
-// console.log(drink2);
+// // console.log(drink2);
 
-console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+// console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+
+/**********************************************************************************
+Chapter 01 - Lesson 025: Coding Challenge
+**********************************************************************************/
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary
+operator!)
+
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300
+GOOD LUCK
+*/
+
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(
+    `The bill was ${bill}, the tip was ${tip} and the total value is ${
+        bill + tip
+    }`
+);
