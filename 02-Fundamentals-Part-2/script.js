ccb03e995f3711203e149ac273aff3728853cb66
jeffersonrolino <jefferson.rolino@gmail.com>
2025-05-01 08:42:16 -0300

    Chapter 02 - Lesson 004: Function Declarations vs Expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -20,28 +20,49 @@ Chapter 02 - Lesson 002: Activating Strict Mode
 /**********************************************************************************
 Chapter 02 - Lesson 003: Functions
 **********************************************************************************/
-function logger() {
-    console.log("My name is Jeff");
+// function logger() {
+//     console.log("My name is Jeff");
+// }
+
+// //calling / running / invoking function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// //Calling the function without store the value in a constant or variable
+// console.log(fruitProcessor(5, 0));
+
+// const appleOrangeJuice = fruitProcessor(3, 4);
+// console.log(appleOrangeJuice);
+
+// const number = Number("23");
+// console.log(number);
+
+/**********************************************************************************
+Chapter 02 - Lesson 004: Function Declarations vs Expressions
+**********************************************************************************/
+//Function Declaraction
+//We can call the function declarations before declare it;
+const age1 = calcAge1(1988);
+
+function calcAge1(birthYear) {
+    return 2025 - birthYear;
 }
 
-//calling / running / invoking function
-logger();
-logger();
-logger();
+console.log(age1);
 
-function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
-}
+//Function Expression (Produces a values) - Anonymous Function
+const calcAge2 = function (birthYear) {
+    return 2025 - birthYear;
+};
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-//Calling the function without store the value in a constant or variable
-console.log(fruitProcessor(5, 0));
-
-const appleOrangeJuice = fruitProcessor(3, 4);
-console.log(appleOrangeJuice);
-
-const number = Number("23");
-console.log(number);
+const age2 = calcAge2(1988);
+console.log(age2);
