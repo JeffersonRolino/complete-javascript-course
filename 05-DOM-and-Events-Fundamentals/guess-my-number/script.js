eb54b63f0fb52d93bfcb9ffe8bef285a6c39a241
jeffersonrolino <jefferson.rolino@gmail.com>
2025-05-21 16:11:26 -0300

    Chapter 05 - Lesson 007: Implementing the Game Logic

--- 05-DOM-and-Events-Fundamentals/guess-my-number/script.js
+++ 05-DOM-and-Events-Fundamentals/guess-my-number/script.js
@@ -1,10 +1,39 @@
 "use strict";
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 10;
+
+document.querySelector(".number").textContent = secretNumber;
+const message = document.querySelector(".message");
+const scoreElement = document.querySelector(".score");
+
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
     console.log(guess, typeof guess);
 
     if (!guess) {
-        document.querySelector(".message").textContent = "No number!";
+        message.textContent = "No number!";
+    } else if (guess === secretNumber) {
+        message.textContent = "Correct Number!";
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            score--;
+            scoreElement.textContent = score;
+            message.textContent = "Too high!";
+        } else {
+            score = 0;
+            scoreElement.textContent = score;
+            message.textContent = "Game Over!";
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            score--;
+            scoreElement.textContent = score;
+            message.textContent = "Too low!";
+        } else {
+            score = 0;
+            scoreElement.textContent = score;
+            message.textContent = "Game Over!";
+        }
     }
 });
