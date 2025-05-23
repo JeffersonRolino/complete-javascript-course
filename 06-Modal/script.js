f76a7db107fee99bfb63b0aab58b919e7d26d368
jeffersonrolino <jefferson.rolino@gmail.com>
2025-05-22 22:59:24 -0300

    Chapter 06 - Lesson 003: Working with classes

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,9 +5,20 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-console.log(btnsOpenModal);
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
     const button = btnsOpenModal[i];
-    console.log(button.textContent);
+    button.addEventListener("click", openModal);
 }
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
