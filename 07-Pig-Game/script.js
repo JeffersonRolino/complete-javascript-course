f79c3c2fc3ccc864dfbfbee2410bf3da374de4e7
jeffersonrolino <jefferson.rolino@gmail.com>
2025-05-25 15:50:40 -0300

    Chapter 07 - Lesson 003: Rolling the Dice

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -3,9 +3,40 @@
 // Selecting score elements
 const score0Element = document.querySelector("#score--0");
 const score1Element = document.querySelector("#score--1");
+
+const current0Element = document.querySelector("#current--0");
+const current1Element = document.querySelector("#current--1");
+
 const diceElement = document.querySelector(".dice");
+
+//Selecting buttons elements
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 
 diceElement.classList.add("hidden");
+
+let currentScore = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+    //1. Generating a random dice roll
+    const diceRoll = Math.trunc(Math.random() * 6) + 1;
+    console.log(diceRoll);
+
+    //2. Display dice
+    diceElement.classList.remove("hidden");
+    diceElement.src = `dice-${diceRoll}.png`;
+
+    //3. CHeck for rolled 1: if true, switch to next player
+    if (diceRoll !== 1) {
+        //Add dice to current score
+        currentScore += diceRoll;
+        current0Element.textContent = currentScore;
+    } else {
+        //Switch to next player
+    }
+});
