0c60d49981d279b02eb83eb36ffe70cf4d6e1359
jeffersonrolino <jefferson.rolino@gmail.com>
2025-05-26 01:32:17 -0300

    Chapter 07 - Lesson 004: Switching the Active Player

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 // Selecting score elements
+const player0Element = document.querySelector(".player--0");
+const player1Element = document.querySelector(".player--1");
+
 const score0Element = document.querySelector("#score--0");
 const score1Element = document.querySelector("#score--1");
 
@@ -19,7 +22,9 @@ score1Element.textContent = 0;
 
 diceElement.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener("click", function () {
@@ -35,8 +40,15 @@ btnRoll.addEventListener("click", function () {
     if (diceRoll !== 1) {
         //Add dice to current score
         currentScore += diceRoll;
-        current0Element.textContent = currentScore;
+        document.querySelector(`#current--${activePlayer}`).textContent =
+            currentScore;
     } else {
         //Switch to next player
+        currentScore = 0;
+        document.querySelector(`#current--${activePlayer}`).textContent =
+            currentScore;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0Element.classList.toggle("player--active");
+        player1Element.classList.toggle("player--active");
     }
 });
