f0e575fcf01a0f5cb928d58c9b02af61c3f74e8c
jeffersonrolino <jefferson.rolino@gmail.com>
2025-05-30 09:47:30 -0300

    Chapter 08 - Lesson 011: The this Keyword in Practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,45 @@
 "use strict";
+
+/**********************************************************************************
+ * Chapter 08 - Lesson 011: The this Keyword in Practice
+ **********************************************************************************/
+console.log(this);
+
+//this Keyword in a Function
+const calcAge = function (birthYear) {
+    console.log(2025 - birthYear);
+    console.log(this);
+};
+
+calcAge(1988);
+
+//this Keyword in a Arrow Function
+const calcAgeArrow = (birthYear) => {
+    console.log(2025 - birthYear);
+    console.log(this);
+};
+
+calcAgeArrow(1980);
+
+//this Keyword in a Object
+const jonas = {
+    birthYear: 1991,
+    calcAge: function () {
+        console.log(2025 - this.birthYear);
+        console.log(this);
+    },
+};
+
+jonas.calcAge();
+
+const matilda = {
+    birthYear: 1998,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+console.log(f);
+
+// f();
