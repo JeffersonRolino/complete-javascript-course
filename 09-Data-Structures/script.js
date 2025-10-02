105597ef6cf52e2d5a4d3baea9a415539833668c
jeffersonrolino <jefferson.rolino@gmail.com>
2025-10-02 07:59:19 -0300

    Chapter 09 - Lesson 014: Looping Objects, Keys, Values and Entries

--- 09-Data-Structures/script.js
+++ 09-Data-Structures/script.js
@@ -459,32 +459,59 @@ Your tasks:
 /**********************************************************************************
  * Chapter 09 - Lesson 013: Optional Chaining
  **********************************************************************************/
-//Old way
-if (restaurant.openingHours.mon) {
-    console.log(restaurant.openingHours.mon.open);
+// //Old way
+// if (restaurant.openingHours.mon) {
+//     console.log(restaurant.openingHours.mon.open);
+// }
+
+// //New way ES2020 (returns undefined)
+// console.log(restaurant.openingHours.mon?.open);
+
+// //Practice
+// const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+// for (const day of days) {
+//     const open = restaurant.openingHours[day]?.open ?? "closed";
+//     console.log(`On ${day}, we open at ${open}`);
+// }
+
+// //Methods
+// console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+
+// console.log(restaurant.orderPizza?.(0, 1) ?? "Method does not exist");
+
+// //Arrays
+// const users = [
+//     { name: "Steve", email: "steve@gmail.com" },
+//     { name: "Antony", email: "tony@gmail.com" },
+//     { name: "Peter", email: "peter@gmail.com" },
+// ];
+
+// console.log(users[1]?.name ?? "User array empty");
+
+/**********************************************************************************
+ * Chapter 09 - Lesson 014: Looping Objects - Keys, Values and Entries
+ **********************************************************************************/
+// PROPERTIES KEYS
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+    openStr += `${day}, `;
 }
 
-//New way ES2020 (returns undefined)
-console.log(restaurant.openingHours.mon?.open);
+console.log(openStr);
 
-//Practice
-const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+// PROPERTIES VALUES
+const values = Object.values(openingHours);
+console.log(values);
 
-for (const day of days) {
-    const open = restaurant.openingHours[day]?.open ?? "closed";
-    console.log(`On ${day}, we open at ${open}`);
+// OBJECT ENTRIES
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+    console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
-
-//Methods
-console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
-
-console.log(restaurant.orderPizza?.(0, 1) ?? "Method does not exist");
-
-//Arrays
-const users = [
-    { name: "Steve", email: "steve@gmail.com" },
-    { name: "Antony", email: "tony@gmail.com" },
-    { name: "Peter", email: "peter@gmail.com" },
-];
-
-console.log(users[1]?.name ?? "User array empty");
