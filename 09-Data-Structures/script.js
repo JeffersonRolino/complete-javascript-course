36a956757bd19a57cf8e6704ad1042465311cf2e
jeffersonrolino <jefferson.rolino@gmail.com>
2025-10-02 06:59:08 -0300

    Chapter 09 - Lesson 013: Optional Chaining

--- 09-Data-Structures/script.js
+++ 09-Data-Structures/script.js
@@ -454,4 +454,37 @@ Your tasks:
 /**********************************************************************************
  * Chapter 09 - Lesson 012: Enhanced Object Literals
  **********************************************************************************/
-console.log(restaurant);
+// console.log(restaurant);
+
+/**********************************************************************************
+ * Chapter 09 - Lesson 013: Optional Chaining
+ **********************************************************************************/
+//Old way
+if (restaurant.openingHours.mon) {
+    console.log(restaurant.openingHours.mon.open);
+}
+
+//New way ES2020 (returns undefined)
+console.log(restaurant.openingHours.mon?.open);
+
+//Practice
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+    const open = restaurant.openingHours[day]?.open ?? "closed";
+    console.log(`On ${day}, we open at ${open}`);
+}
+
+//Methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+
+console.log(restaurant.orderPizza?.(0, 1) ?? "Method does not exist");
+
+//Arrays
+const users = [
+    { name: "Steve", email: "steve@gmail.com" },
+    { name: "Antony", email: "tony@gmail.com" },
+    { name: "Peter", email: "peter@gmail.com" },
+];
+
+console.log(users[1]?.name ?? "User array empty");
