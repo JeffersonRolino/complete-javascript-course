925cc70bb7a908e0c722b0b694fe242a5d2fa13b
jeffersonrolino <jefferson.rolino@gmail.com>
2025-10-03 09:30:14 -0300

    Chapter 09 - Lesson 020: Coding Challenge #3

--- 09-Data-Structures/script.js
+++ 09-Data-Structures/script.js
@@ -689,41 +689,91 @@ Your tasks:
 /**********************************************************************************
  * Chapter 09 - Lesson 018: Maps Iteration
  **********************************************************************************/
-const question = new Map([
-    ["question", "What is the best programming language in the world?"],
-    [1, "C"],
-    [2, "Java"],
-    [3, "JavaScript"],
-    ["correct", 3],
-    [true, "Correct!"],
-    [false, "Wrong!"],
+// const question = new Map([
+//     ["question", "What is the best programming language in the world?"],
+//     [1, "C"],
+//     [2, "Java"],
+//     [3, "JavaScript"],
+//     ["correct", 3],
+//     [true, "Correct!"],
+//     [false, "Wrong!"],
+// ]);
+
+// console.log(question);
+
+// // Convert Object to Map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+
+// console.log(hoursMap);
+
+// //Quiz app
+// console.log(question.get("question"));
+
+// for (const [key, value] of question) {
+//     if (typeof key === "number") {
+//         console.log(`Answer ${key}: ${value}`);
+//     }
+// }
+
+// // const answer = Number(prompt("Your answer"));
+// const answer = 3;
+// console.log(answer);
+
+// console.log(question.get(answer === question.get("correct")));
+
+// //Convert Map to Array
+// console.log([...question]);
+// console.log([...question.entries()]);
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
+
+/**********************************************************************************
+ * Chapter 09 - Lesson 019: Coding Challenge #3
+ **********************************************************************************/
+/*
+Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+Your tasks:
+    1. Create an array 'events' of the different game events that happened (no duplicates).
+    2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+    3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes).
+    4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17: ⚽ GOAL
+*/
+
+const gameEvents = new Map([
+    [17, "⚽ GOAL"],
+    [36, "🔁 Substitution"],
+    [47, "⚽ GOAL"],
+    [61, "🔁 Substitution"],
+    [64, "🔶 Yellow card"],
+    [69, "🔴 Red card"],
+    [70, "🔁 Substitution"],
+    [72, "🔁 Substitution"],
+    [76, "⚽ GOAL"],
+    [80, "⚽ GOAL"],
+    [92, "🔶 Yellow card"],
 ]);
 
-console.log(question);
+//Task 1:
+const events = [...new Set(gameEvents.values())];
+console.log(events);
 
-// Convert Object to Map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
+//Task 2:
+gameEvents.delete(64);
 
-console.log(hoursMap);
+//Task 3:
+console.log(
+    `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
 
-//Quiz app
-console.log(question.get("question"));
-
-for (const [key, value] of question) {
-    if (typeof key === "number") {
-        console.log(`Answer ${key}: ${value}`);
-    }
+//Task 4:
+for (const [key, value] of gameEvents) {
+    console.log(
+        key <= 45
+            ? `[FIRST HALF] ${key}: ${value}`
+            : `[SECOND HALF] ${key}: ${value}`
+    );
 }
 
-// const answer = Number(prompt("Your answer"));
-const answer = 3;
-console.log(answer);
-
-console.log(question.get(answer === question.get("correct")));
-
-//Convert Map to Array
-console.log([...question]);
-console.log([...question.entries()]);
-console.log([...question.keys()]);
-console.log([...question.values()]);
+console.log(gameEvents);
