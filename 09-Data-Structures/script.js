dea42f099dafdb8011e9ef523c0ec813f50f5c40
jeffersonrolino <jefferson.rolino@gmail.com>
2025-10-20 22:47:15 -0300

    Chapter 09 - Lesson 021: Working With Strings - Part 1

--- 09-Data-Structures/script.js
+++ 09-Data-Structures/script.js
@@ -741,39 +741,88 @@ Your tasks:
     4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this: [FIRST HALF] 17: ⚽ GOAL
 */
 
-const gameEvents = new Map([
-    [17, "⚽ GOAL"],
-    [36, "🔁 Substitution"],
-    [47, "⚽ GOAL"],
-    [61, "🔁 Substitution"],
-    [64, "🔶 Yellow card"],
-    [69, "🔴 Red card"],
-    [70, "🔁 Substitution"],
-    [72, "🔁 Substitution"],
-    [76, "⚽ GOAL"],
-    [80, "⚽ GOAL"],
-    [92, "🔶 Yellow card"],
-]);
+// const gameEvents = new Map([
+//     [17, "⚽ GOAL"],
+//     [36, "🔁 Substitution"],
+//     [47, "⚽ GOAL"],
+//     [61, "🔁 Substitution"],
+//     [64, "🔶 Yellow card"],
+//     [69, "🔴 Red card"],
+//     [70, "🔁 Substitution"],
+//     [72, "🔁 Substitution"],
+//     [76, "⚽ GOAL"],
+//     [80, "⚽ GOAL"],
+//     [92, "🔶 Yellow card"],
+// ]);
 
-//Task 1:
-const events = [...new Set(gameEvents.values())];
-console.log(events);
+// //Task 1:
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
 
-//Task 2:
-gameEvents.delete(64);
+// //Task 2:
+// gameEvents.delete(64);
 
-//Task 3:
-console.log(
-    `An event happened, on average, every ${90 / gameEvents.size} minutes`
-);
+// //Task 3:
+// console.log(
+//     `An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
 
-//Task 4:
-for (const [key, value] of gameEvents) {
-    console.log(
-        key <= 45
-            ? `[FIRST HALF] ${key}: ${value}`
-            : `[SECOND HALF] ${key}: ${value}`
-    );
-}
+// //Task 4:
+// for (const [key, value] of gameEvents) {
+//     console.log(
+//         key <= 45
+//             ? `[FIRST HALF] ${key}: ${value}`
+//             : `[SECOND HALF] ${key}: ${value}`
+//     );
+// }
 
-console.log(gameEvents);
+// console.log(gameEvents);
+
+/**********************************************************************************
+ * Chapter 09 - Lesson 019: Coding Challenge #3
+ **********************************************************************************/
+const airline = "TAP Air Portugal";
+const plane = "A320";
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log("B737"[0]);
+console.log(airline.length);
+console.log("B737".length);
+
+console.log(airline.indexOf("r"));
+console.log(airline.lastIndexOf("r"));
+console.log(airline.indexOf("Portugal"));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7)); //end index is exclusive
+
+//extracting the first word
+console.log(airline.slice(0, airline.indexOf(" ")));
+
+//extracting the last word
+console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+    //B and E are middle seats
+    const letter = seat.slice(-1);
+    if (letter === "B" || letter === "E") {
+        console.log("You got the middle seat");
+    } else {
+        console.log("You got lucky!");
+    }
+};
+
+checkMiddleSeat("11B");
+checkMiddleSeat("23C");
+checkMiddleSeat("42A");
+checkMiddleSeat("17E");
+
+console.log(new String("Harry"));
+console.log(typeof new String("Harry"));
+
+console.log(typeof new String("Harry").slice(1));
